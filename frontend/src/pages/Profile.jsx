0aa4259cee17c2cb4f07ef7fc69c0abe5cd88803
IL-iliesse78 <86209795+IL-iliesse78@users.noreply.github.com>
2025-10-29 10:18:33 +0100

    - Structure de la page Profil (layout général avec emplacements graphiques et cartes) - CSS temporaire du profil conforme à la maquette (grille 3 colonnes + alignement radialchart), pas encore responsive.

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -2,13 +2,12 @@ import { useEffect, useState } from "react";
 import { getUserData } from "../services/api";
 import "../styles/Profile.css";
 
-
 function Profile() {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
-      const userData = await getUserData(12); // ici je mets mon id 12 = Karl
+      const userData = await getUserData(12); // id 12 = Karl
       setUser(userData);
     }
     fetchData();
@@ -17,12 +16,30 @@ function Profile() {
   if (!user) return <p>Chargement...</p>;
 
   return (
-    <div className="profile">
-      <h1>
-        Bonjour <span>{user.userInfos.firstName}</span>
-      </h1>
-      <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
-    </div>
+    <main className="profile-page">
+      <section className="profile-header">
+        <h1>
+          Bonjour <span className="user-name">{user.userInfos.firstName}</span>
+        </h1>
+        <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+      </section>
+
+      <section className="profile-content">
+        <div className="charts">
+          <div className="barchart">BarChart activité quotidienne</div>
+          <div className="linechart">LineChart durée moyenne des sessions</div>
+          <div className="radarchart">RadarChart types d’activité</div>
+          <div className="radialchart">RadialBarChart score moyen</div>
+        </div>
+
+        <aside className="key-data">
+          <div className="card">Calories</div>
+          <div className="card">Protéines</div>
+          <div className="card">Glucides</div>
+          <div className="card">Lipides</div>
+        </aside>
+      </section>
+    </main>
   );
 }
 
